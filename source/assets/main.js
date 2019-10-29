0cd0a32a9f350a09200502b4fa1092376e4dc1fb
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2019-10-29 15:59:39 +0100

    --wip--

--- source/assets/main.js
+++ source/assets/main.js
@@ -3,6 +3,7 @@ import "./stylesheets/design-language-legacy.css.sass"
 
 // New css
 import "./stylesheets/design-system.css"
+// import "appsignal-design-system/css/design-system.css"
 
 
 // All javascript
